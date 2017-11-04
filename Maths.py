8f89e791eee2ada744b06a279f885e4f050618ca
Dylan <Dylan@DESKTOP-4M95CH4>
2017-11-04 19:35:31 +0000

    Basic interface to be improved on later

--- Maths.py
+++ Maths.py
@@ -1,1 +1,19 @@
-print("Hello World")
+import tkinter as Tk
+from tkinter import *
+ 
+window= Tk()
+window.title("Maths department")
+window.geometry("400x400")
+
+
+
+
+
+widget = Label(window, text= 'Do you want revision notes or questions?')
+widget2= Button(window, text='Revision notes')
+widget.pack()
+widget.mainloop()
+widget2.pack()
+widget2.mainloop()
+
+

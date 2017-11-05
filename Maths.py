9e82c20a6bd53065a05db6e230b62536c3d413eb
Dylan <Dylan@DESKTOP-4M95CH4>
2017-11-05 10:22:29 +0000

    Finished maths gui

--- Maths.py
+++ Maths.py
@@ -3,17 +3,21 @@ from tkinter import *
  
 window= Tk()
 window.title("Maths department")
-window.geometry("400x400")
-
+window.geometry("400x200")
 
 
 
 
 widget = Label(window, text= 'Do you want revision notes or questions?')
 widget2= Button(window, text='Revision notes')
+widget3= Button(window, text='Questions')
 widget.pack()
-widget.mainloop()
 widget2.pack()
-widget2.mainloop()
+widget3.pack()
+window.mainloop()
 
+def quit_window(self):
+    self.Q1()
+    self.window.destroy()
 
+  

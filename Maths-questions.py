639c1a3e1fee91d349c4db0f10bc95049029532c
Dylan <Dylan@DESKTOP-4M95CH4>
2017-11-09 19:12:03 +0000

    sdfsdc

--- Maths-questions.py
+++ Maths-questions.py
@@ -1,21 +1,24 @@
-
+import random
 import tkinter as Tk
 from tkinter import *
 import json
 from pprint import pprint
 global data
 
-
-
 Index1 = open("Resources/Index.json","r")
 JsonIndex= json.load(Index1)
-question1=JsonIndex["Index"]["1"]["q"]
 
 
- 
 window2= Tk()
 window2.title("Maths questions")
 window2.geometry("400x400")
+
+def get_MathsQuestions():
+    index = JsonIndex["Index"]      
+    question_selector = index[(random.randint(1,index))]
+    Real_question_selector = str(question_selector)
+    final_question = [Real_question_selector["a"], Real_question_selector["b"], Real_question_selector["c"], Real_question_selector["d"], Real_question_selector["q"]]
+    return final_question
 
 def Correct():
     from tkinter import messagebox
@@ -25,17 +28,22 @@ def Incorrect():
     from tkinter import messagebox
     messagebox.showinfo(title="Incorrect answer", message= "Wrong! Remember, 2 is a prime number. The other primes in the range 1-10 are 3,5,7. 4/10= 40%.")
 
+
+questions=get_MathsQuestions()
+MathsQuestion_label=Label(window2, text=questions[4], font=40)
+
+
 text_file= open("Resources/Maths Questions.txt", "r")
 text_file2= open("Resources/Possible answers.txt", "r")
 text_file3= open("Resources/Possible answers 2.txt","r")
 text_file4= open("Resources/Possible answers 3.txt","r")
 text_file5= open("Resources/Possible answers 4.txt","r")
-Lines= Label(window2,text=question1)
-Button1= Button(window2,text=text_file2.read(6), command= Correct)
+Button1= Button(window2,text=answera1, command= Correct)
 Button2= Button(window2,text=text_file3.read(6), command= Incorrect)
 Button3= Button(window2,text=text_file4.read(6), command=Incorrect)
 Button4= Button(window2,text=text_file5.read(6), command=Incorrect)
-Lines.pack()
+
+MathsQuestion_label.pack()
 Button1.pack()
 Button2.pack()
 Button3.pack()

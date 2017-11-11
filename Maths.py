d36645e386101e89d91800276406ed960e5f418a
Dylan <Dylan@DESKTOP-4M95CH4>
2017-11-11 14:34:41 +0000

    asdadwasd

--- Maths.py
+++ Maths.py
@@ -1,23 +1,23 @@
 import tkinter as Tk
 from tkinter import *
- 
+
+
 window= Tk()
 window.title("Maths department")
 window.geometry("400x200")
 
-
+def openquestions():
+    import Maths_questions
 
 
 widget = Label(window, text= 'Do you want revision notes or questions?')
 widget2= Button(window, text='Revision notes')
-widget3= Button(window, text='Questions')
+widget3= Button(window, text='Randomly selected questions',command=openquestions)
 widget.pack()
 widget2.pack()
 widget3.pack()
 window.mainloop()
 
-def quit_window(self):
-    self.Q1()
-    self.window.destroy()
+
 
   

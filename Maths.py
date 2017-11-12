85eb3d4512a935419a985dce84566a276e26e418
Dylan <Dylan@DESKTOP-4M95CH4>
2017-11-12 11:37:35 +0000

    All the maths stuff

--- Maths.py
+++ Maths.py
@@ -8,11 +8,14 @@ window.geometry("400x200")
 
 def openquestions():
     window.destroy()
-    import Maths_questions
-    
+    import Maths_Questions
+
+def openrevision():
+    window.destroy()
+    import Revision_notes_gui   
 
 widget = Label(window, text= 'Do you want revision notes or questions?')
-widget2= Button(window, text='Revision notes')
+widget2= Button(window, text='Revision notes',command=openrevision)
 widget3= Button(window, text='Randomly selected questions',command=openquestions)
 widget.pack()
 widget2.pack()
--- Resources/Revision_notes_gui.py
+++
@@ -1,6 +0,0 @@
-import tkinter as Tk
-from tkinter import *
-window3=Tk()
-Label1=Label(window3,text="Which mathematics subject do you want?")
-AnglesButton=Button(window3,text="Angles")
-TrianglesButton=Button(window3,text="Triangles")

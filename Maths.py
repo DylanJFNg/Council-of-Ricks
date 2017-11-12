01520f444da491920dbf8442b990fa5aa2052eaf
DylanJFNg <33378606+DylanJFNg@users.noreply.github.com>
2017-11-12 11:40:10 +0000

    Update Maths.py

--- Maths.py
+++ Maths.py
@@ -1,23 +1,25 @@
 import tkinter as Tk
 from tkinter import *
- 
+
+
 window= Tk()
-window.title("Maths department")
+window.title("Mathematics department")
 window.geometry("400x200")
 
+def openquestions():
+    window.destroy()
+    import Maths_Questions
 
-
+def openrevision():
+    window.destroy()
+    import Revision_notes_gui   
 
 widget = Label(window, text= 'Do you want revision notes or questions?')
-widget2= Button(window, text='Revision notes')
-widget3= Button(window, text='Questions')
+widget2= Button(window, text='Revision notes',command=openrevision)
+widget3= Button(window, text='Randomly selected questions',command=openquestions)
 widget.pack()
 widget2.pack()
 widget3.pack()
 window.mainloop()
-
-def quit_window(self):
-    self.Q1()
-    self.window.destroy()
 
   

a292e70c0e31f6d8af7aaa93cbbb6a8bc809dc36
Dylan <Dylan@DESKTOP-4M95CH4>
2017-11-13 19:09:25 +0000

    dedede

--- Maths.py
+++ Maths.py
@@ -6,31 +6,31 @@ import json
 from pprint import pprint
 from random import randint, shuffle
 from builtins import str
-window= Tk()
-window.title("Mathematics department")
-window.geometry("400x200")
 
-def InitMaths():
-    widget = Label(window, text= 'Do you want revision notes or questions?')
-    widget2= Button(window, text='Revision notes',command=Revision)
-    widget3= Button(window, text='Randomly selected questions',command=MathsQuestions)
+def InitMaths2():
+    i=0
+    while i<1:
+        Realwindow.destroy()
+        i=i+1
+    Realwindow=Tk()
+    Realwindow.title("Mathematics Department")
+    Realwindow.geometry("400x200")
+    Realwindow.configure(background='Dark Blue')
+    widget = Label(Realwindow, text= 'Do you want revision notes or questions?',bg='Light Blue')
+    widget2= Button(Realwindow, text='Revision notes',bg='Cyan',fg='Red',command=Revision)
+    widget3= Button(Realwindow, text='Randomly selected questions',bg='Cyan',fg='Red',command=MathsQuestions)
     widget.pack()
     widget2.pack()
     widget3.pack()
-    window.mainloop()
-
-
+    Realwindow.mainloop()
 def Revision():
-    window.destroy()
     window3=Tk()
     window3.title("Revision")
     window3.geometry("1000x800")
-
-    
+    window3.configure(background='Dark Blue')
     def back():
         window3.destroy()
-        InitMaths()
-
+        InitMaths2()
     TrianglesText=open("Resources\TrianglesAndTrigonometricRatios.txt","r")
     SequencesText=open("Resources\SequencesRevision.txt","r")
     AnglesText=open("Resources\AnglesRevision.txt","r")
@@ -51,7 +51,6 @@ def Revision():
 
     Prime=PrimeText.read()
     Label6=Label(window3,text=Prime,font=("none",10))
-
 
 
 
@@ -102,7 +101,7 @@ def Revision():
         BackButton2=Button(window3,text="Back",command=back3)
         BackButton2.pack()
         window3.mainloop()
-        
+            
     def ReadIndices():
         def back4():
             Label5.pack_forget()
@@ -126,7 +125,7 @@ def Revision():
         BackButton2=Button(window3,text="Back",command=back4)
         BackButton2.pack()
         window3.mainloop()  
-        
+            
     def ReadSequences():
         def back5():
             Label4.pack_forget()
@@ -150,7 +149,7 @@ def Revision():
         BackButton2=Button(window3,text="Back",command=back5)
         BackButton2.pack()
         window3.mainloop() 
-    
+        
     def ReadPrime():
         def back6():
             Label6.pack_forget()
@@ -174,15 +173,15 @@ def Revision():
         BackButton2=Button(window3,text="Back",command=back6)
         BackButton2.pack()
         window3.mainloop() 
-    
+
      
-    Label1=Label(window3,text="Which mathematics subject do you want?")
-    AnglesButton=Button(window3,text="Angles",command=ReadAngles)
-    TrianglesButton=Button(window3,text="Triangles and trigonometrical ratios",command=ReadTriangles)
-    IndicesButton=Button(window3,text="Indices",command=ReadIndices)
-    SequencesButton=Button(window3,text="Sequences",command=ReadSequences)
-    PrimesButton=Button(window3,text="Prime Numbers",command=ReadPrime)
-    BackButton=Button(window3,text="Back",command=back)
+    Label1=Label(window3,text="Which mathematics subject do you want?",bg='cyan')
+    AnglesButton=Button(window3,text="Angles",command=ReadAngles,bg='cyan')
+    TrianglesButton=Button(window3,text="Triangles and trigonometrical ratios",command=ReadTriangles,bg='cyan')
+    IndicesButton=Button(window3,text="Indices",command=ReadIndices,bg='cyan')
+    SequencesButton=Button(window3,text="Sequences",command=ReadSequences,bg='cyan')
+    PrimesButton=Button(window3,text="Prime Numbers",command=ReadPrime,bg='cyan')
+    BackButton=Button(window3,text="Back",command=back,bg='Black',fg='White')
     Label1.pack()
     IndicesButton.pack()
     SequencesButton.pack()
@@ -191,16 +190,14 @@ def Revision():
     TrianglesButton.pack()
     BackButton.pack()
     window3.mainloop()
-
+    Realwindow.destroy()
 def MathsQuestions():
-    window.destroy()
     Index1 = open("Resources/Index.json","r")
     JsonIndex= json.load(Index1)
-
     window2= Tk()
     window2.title("Maths questions")
     window2.geometry("700x700")
-
+    window2.configure(background='Dark Blue')
     def get_MathsQuestions():
         index = JsonIndex["Index"]  
         RealIndex = int(index)  
@@ -208,18 +205,17 @@ def MathsQuestions():
         Real_question_selector = JsonIndex[tmp]   
         final_question = [Real_question_selector["a"], Real_question_selector["b"], Real_question_selector["c"], Real_question_selector["d"], Real_question_selector["q"]]
         return final_question
-
+        
     def Correct():
         from tkinter import messagebox
         messagebox.showinfo(title="You're right!", message="Well done! You got the answer correct!")
         window2.destroy()
-        InitMaths()
-        
+        InitMaths2()
+            
     def Incorrect():
         from tkinter import messagebox
         messagebox.showinfo(title="Incorrect answer", message= "That's the wrong answer. Try again.")
-
-
+    
     
     questions=get_MathsQuestions()
 
@@ -236,8 +232,26 @@ def MathsQuestions():
     Button4.pack()
     window2.mainloop()
 
+Realwindow=Tk()
 
+def InitMaths():      
+    Realwindow.title("Mathematics Department")
+    Realwindow.geometry("400x200")
+    Realwindow.configure(background='Dark Blue')
+    widget = Label(Realwindow, text= 'Do you want revision notes or questions?',bg='Light Blue')
+    widget2= Button(Realwindow, text='Revision notes',bg='Cyan',fg='Red',command=Revision)
+    widget3= Button(Realwindow, text='Randomly selected questions',bg='Cyan',fg='Red',command=MathsQuestions)
+    widget.pack()
+    widget2.pack()
+    widget3.pack()
+    Realwindow.mainloop()
+    
 InitMaths()
+        
+
+
+
+
+
+
   
-
-

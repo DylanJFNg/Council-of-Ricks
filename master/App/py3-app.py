10b59d7a0cc27e193c6b60b8cf642fb9eb96dab5
Dylan <Dylan@DESKTOP-4M95CH4>
2017-12-03 20:08:40 +0000

    waeafe

--- master/App/py3-app.py
+++ master/App/py3-app.py
@@ -70,13 +70,18 @@ def english():
             global score, count
             # get globals
             qq = get_questions("english.json")
+            score_readout = Label(mat, text="Score: " + str(score) + "/" + str(count), font=(None,20),fg="blue",bg="yellow")
+            score_readout.pack(side="top", anchor="e")
             # retrive questions from json parcer with inputed json files
-            eh1 = Label(mat, text="Question " + str(count+1), font=(None,10),fg="blue")
-            eh1.pack()
-            score_readout = Label(mat, text="Score: " + str(score) + "/" + str(count), font=(None,10),fg="blue")
-            score_readout.pack()
-            question_label = Label(mat, text=qq[4], font=(None,15),fg="blue")
-            question_label.pack()
+            eh1 = Label(mat, text="Question " + str(count+1), font=(None,20),fg="blue",bg="yellow")
+            eh1.pack(side="top", anchor="w")
+ 
+            spacing_label = Label(mat, text="", font=(None,15),fg="blue",bg="yellow")
+            spacing_label.pack()
+            
+            question_label = Label(mat, text=qq[4], font=(None,15),fg="blue",bg="yellow")
+            question_label.pack(side="top", anchor="w")
+
 
             def correct():
                 global score, count
@@ -118,14 +123,15 @@ def english():
                 score_readout.pack_forget()
                 eh1.pack_forget()
                 question_label.pack_forget()
-            mat_b1 = Button(mat, text=qq[0], command=correct)
-            mat_b2 = Button(mat, text=qq[1], command=incorrect)
-            mat_b3 = Button(mat, text=qq[2], command=incorrect)
-            mat_b4 = Button(mat, text=qq[3], command=incorrect)
+                spacing_label.pack_forget()
+            mat_b1 = Button(mat, text=qq[0], command=correct,font=(None,20),width=30)
+            mat_b2 = Button(mat, text=qq[1], command=incorrect,font=(None,20),width=30)
+            mat_b3 = Button(mat, text=qq[2], command=incorrect,font=(None,20),width=30)
+            mat_b4 = Button(mat, text=qq[3], command=incorrect,font=(None,20),width=30)
             bttns = [mat_b1, mat_b2, mat_b3, mat_b4]
             shuffle(bttns)
             for mat_ in bttns:
-                mat_.pack()
+                mat_.pack(side="top", anchor="w")
         ask_question()
         mat.mainloop()
 
@@ -263,10 +269,10 @@ def math():
                 score_readout.pack_forget()
                 eh1.pack_forget()
                 question_label.pack_forget()
-            mat_b1 = Button(mat, text=qq[0], command=correct)
-            mat_b2 = Button(mat, text=qq[1], command=incorrect)
-            mat_b3 = Button(mat, text=qq[2], command=incorrect)
-            mat_b4 = Button(mat, text=qq[3], command=incorrect)
+            mat_b1 = Button(mat, text=qq[0], command=correct,font=(None,30))
+            mat_b2 = Button(mat, text=qq[1], command=incorrect,font=(None,30))
+            mat_b3 = Button(mat, text=qq[2], command=incorrect,font=(None,30))
+            mat_b4 = Button(mat, text=qq[3], command=incorrect,font=(None,30))
             bttns = [mat_b1, mat_b2, mat_b3, mat_b4]
             shuffle(bttns)
             for mat_ in bttns:
